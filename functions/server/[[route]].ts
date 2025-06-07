16669ee0fff292b56f851012464ed11874a5c05c
adebola-io <adebolaakomolafe@gmail.com>
2025-06-07 09:10:59 +0100

    feat: Migrate Hono server to file-based routing

--- functions/server/[[route]].ts
+++ functions/server/[[route]].ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/cloudflare-pages';
+import waitingList from '@recoin/api/waiting-list/server';
 
 const app = new Hono();
 
 app.get('/', (c) => {
    return c.text('The recoin server is running!');
 });
+app.route('/waiting-list', waitingList);
 
 export const onRequest = handle(app);
--- functions/server/waiting-list.ts
+++
@@ -1,4 +0,0 @@
-import { handle } from 'hono/cloudflare-pages';
-import waitingList from '@recoin/api/waiting-list/server';
-
-export const onRequest = handle(waitingList);
