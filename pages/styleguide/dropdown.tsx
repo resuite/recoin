a4b6a451d622ac9f52c184c1dacaade0c8deebbc
Sefunmi <adebolaakomolafe@gmail.com>
2025-08-13 17:54:18 +0100

    Add Dropdown styleguide page

---
+++ pages/styleguide/dropdown.tsx
@@ -0,0 +1,37 @@
+import { Dropdown } from '@/components/ui/dropdown'
+import { Cell } from 'retend'
+
+const DropdownTest = () => {
+   const options = [
+      { label: 'Apple', value: 'apple' },
+      { label: 'Mango', value: 'mango' },
+      { label: 'Banana', value: 'banana' },
+      { label: 'Pawpaw', value: 'pawpaw' },
+      { label: 'Guava', value: 'guava' },
+      { label: 'Orange', value: 'orange' },
+      { label: 'Pineapple', value: 'pineapple' },
+      { label: 'Strawberry', value: 'strawberry' },
+      { label: 'Blueberry', value: 'blueberry' },
+      { label: 'Watermelon', value: 'watermelon' },
+      { label: 'Grapes', value: 'grapes' },
+      { label: 'Peach', value: 'peach' },
+      { label: 'Cherry', value: 'cherry' },
+      { label: 'Lemon', value: 'lemon' },
+      { label: 'Kiwi', value: 'kiwi' }
+   ]
+
+   const selected = Cell.source(options[0])
+   const selectedLabel = Cell.derived(() => selected.get().label)
+
+   return (
+      <div class='grid gap-0.5 place-items-center place-content-center h-screen w-screen'>
+         <Dropdown selectedOption={selected} options={options} />
+         <span>This is a dropdown test.</span>
+         <span>
+            The selected is <b>{selectedLabel} </b>
+         </span>
+      </div>
+   )
+}
+
+export default DropdownTest
