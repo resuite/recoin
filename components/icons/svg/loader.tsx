50ecdf069c8830ecd29260e37eff0a05d8cfc5fe
Sefunmi <adebolaakomolafe@gmail.com>
2025-11-09 05:22:04 +0100

    Update loader SVG design

--- components/icons/svg/loader.tsx
+++ components/icons/svg/loader.tsx
@@ -6,19 +6,30 @@ export default function Loader(props: IconProps) {
       <div {...props} class={['animate-spin', props.class]}>
          <svg
             style={{ height: '100%', width: '100%' }}
-            viewBox='0 0 14 15'
+            viewBox='0 0 68 68'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'
          >
             <title xmlns='http://www.w3.org/2000/svg'>Loading...</title>
             <rect
-               x='5.24793'
-               y='1.62746'
-               width='8.66667'
-               height='8.66667'
-               transform='rotate(28.3883 5.24793 1.62746)'
+               x='24.5808'
+               y='2.71038'
+               width='46'
+               height='46'
+               rx='9.30715'
+               transform='rotate(28.3883 24.5808 2.71038)'
                stroke='currentColor'
-               stroke-width='1.33333'
+               stroke-width='4'
+            />
+            <rect
+               x='30.3572'
+               y='22.0727'
+               width='17.4248'
+               height='17.4248'
+               rx='5.30828'
+               transform='rotate(28.3883 30.3572 22.0727)'
+               stroke='currentColor'
+               stroke-width='4'
             />
          </svg>
       </div>
