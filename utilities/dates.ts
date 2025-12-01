9c690cb84fd09855399b0f030f8d685c67e781f4
Sefunmi <adebolaakomolafe@gmail.com>
2025-12-01 21:53:52 +0100

    Fix time formatting to use lowercase

--- utilities/dates.ts
+++ utilities/dates.ts
@@ -15,11 +15,13 @@ export function ordinal(n: number) {
 }
 
 export function formatTime(date: Date) {
-   return date.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-   })
+   return date
+      .toLocaleTimeString('en-US', {
+         hour: '2-digit',
+         minute: '2-digit',
+         hour12: true
+      })
+      .toLocaleLowerCase()
 }
 
 export function formatRelativeTime(date: Date) {
